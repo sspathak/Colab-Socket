c3213db25e8758ea1a169f8f7b3a4033e167c425
sspathak <suraj.s.pathak@gmail.com>
2021-05-15 15:24:41 +0400

    restructuring

--- client.py
+++ client.py
@@ -7,6 +7,7 @@ HOST = '127.0.0.1'  # The server's hostname or IP address
 # HOST = '35.209.32.35'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
+
 def split_send_bytes(s, inp):
     data_len = (len(inp))
     header = str(data_len).encode()
@@ -15,6 +16,7 @@ def split_send_bytes(s, inp):
         s.send(inp[i * 1024:i * 1024 + 1024])
     if data_len % 1024 != 0:
         s.send(inp[-(data_len % 1024):])
+
 
 def split_recv_bytes(s):
     dat = b''
@@ -27,15 +29,22 @@ def split_recv_bytes(s):
     return dat
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    data = None
-    s.connect((HOST, PORT))
-    while data != "quit":
-        while (inp := input("Enter text to send:")) == "":
-            continue
-        inp = inp.encode('utf8')
+def test():
+    HOST = input("Enter server IP")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        data = None
+        s.connect((HOST, PORT))
+        while data != "quit":
+            while (inp := input("Enter text to send:")) == "":
+                continue
+            inp = inp.encode('utf8')
 
-        split_send_bytes(s, inp)
-        data = split_recv_bytes(s).decode('utf8')
-        print('Received          :' + data)
-        print(inp.decode('utf8') == data)
+            split_send_bytes(s, inp)
+            data = split_recv_bytes(s).decode('utf8')
+            print('Received          :' + data)
+            print(inp.decode('utf8') == data)
+
+
+if __name__ == "__main__":
+    test()
+
